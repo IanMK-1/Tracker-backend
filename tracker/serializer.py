7adc64061bb054af0f00fc2d7896fbe333507759
ian mwangi <iankimotho233@gmail.com>
2020-03-31 16:28:20 +0300

    Create Profile serializer

--- tracker/serializer.py
+++ tracker/serializer.py
@@ -1,3 +1,11 @@
 from rest_framework import serializers
 from .models import Profile, Device, GPSCoordinate, SecondaryCircuit
 
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('name', 'bio', 'location', 'profile_pic')
+
+
+
