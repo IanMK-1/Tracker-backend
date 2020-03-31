93aa42bf40e958adf52326c78786bb4edd4b02b5
ian mwangi <iankimotho233@gmail.com>
2020-03-31 15:38:18 +0300

    Register the models in admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Device, GPSCoordinate, SecondaryCircuit
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Device)
+admin.site.register(GPSCoordinate)
+admin.site.register(SecondaryCircuit)
