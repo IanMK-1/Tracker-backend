45b240b3d84aa3f770dca693b15fff2c9a3e4645
ian mwangi <iankimotho233@gmail.com>
2020-03-31 17:38:31 +0300

    Correct Profile APIView

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserCreate
+from .views import UserCreate, UpdateUserProfile, UserProfile
 
 urlpatterns = [
     path('user_registration/', UserCreate.as_view(), name='user_create'),
+    path('profile/<int:pk>/', UserProfile.as_view(), name='user_profile'),
+    path('update_profile/<int:pk>/', UpdateUserProfile.as_view(), name='profile_update'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -14,20 +14,26 @@ class UserCreate(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserProfile(generics.RetrieveUpdateAPIView):
+class UserProfile(generics.ListAPIView):
     def get_queryset(self):
         queryset = Profile.objects.filter(user_id=self.kwargs["pk"])
         return queryset
 
     serializer_class = ProfileSerializer
 
+
+class UpdateUserProfile(generics.UpdateAPIView):
     def put(self, request, *args, **kwargs):
         profile = Profile.objects.get(user_id=self.kwargs["pk"])
-        serializer = ProfileSerializer(profile, data=request.data)
+        user = User.objects.filter(pk=self.kwargs["pk"])
+        name = request.data.get("name")
+        bio = request.data.get("bio")
+        location = request.data.get("location")
+        profile_pic = request.data.get("profile_pic")
+        data = {"name": name, "bio": bio, "location": location, "profile_pic": profile_pic, "user": user}
+        serializer = ProfileSerializer(profile, data=data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
