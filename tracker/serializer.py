c5a153366c6f5acffb40acda2e4e3d8190d55ff3
ian mwangi <iankimotho233@gmail.com>
2020-03-31 16:41:55 +0300

    Create User serializer

--- tracker/serializer.py
+++ tracker/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile, Device, GPSCoordinate, SecondaryCircuit
+from django.contrib.auth.models import User
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -26,4 +27,18 @@ class SecondaryCircuitSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
