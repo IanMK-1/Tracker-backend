2296a6a51e2f2fe202f9545fbce5ec009da965a0
ian mwangi <iankimotho233@gmail.com>
2020-03-31 16:47:17 +0300

    Create User APIView

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.views import APIView
+
+from .models import Profile, GPSCoordinate, Device, SecondaryCircuit
+from .serializer import ProfileSerializer, DeviceSerializer, GPSSerializer, SecondaryCircuitSerializer, UserSerializer
+
 
 # Create your views here.
+class UserCreate(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
